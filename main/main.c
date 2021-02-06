3d97ba1a2f19fbfc5d3b57783f702fa458dce099
gkis <gabrielkis65@gmail.com>
2021-02-05 23:05:40 -0300

    Timer_0 - group 0 - configuration

--- main/main.c
+++ main/main.c
@@ -1,14 +1,59 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/timer.h"
+#include "esp_log.h"
 
+#define TIMER_DIVIDER          16
+#define TIMER_SCALE            (TIMER_BASE_CLK / TIMER_DIVIDER)     // convert counter value to seconds
+#define TIMER_INTERVAL_SEC     (1)                                  // sample test interval for the first timer
+#define ALARM_VAL_US           (TIMER_INTERVAL_SEC * TIMER_SCALE)
+#define WITH_RELOAD            1
+
+uint32_t alarm_counter = 0;
+
+/* Rotina de interrupcao */
+static void IRAM_ATTR timer0_ISR(void *ptr)
+{
+    timer_group_clr_intr_status_in_isr(TIMER_GROUP_0, TIMER_0);
+    timer_group_enable_alarm_in_isr(TIMER_GROUP_0, TIMER_0);
+    alarm_counter++;
+}
+
+/* Inicializacao TIMER_0 - group_0 */
+static void config_timer(int timer_idx, bool auto_reload)
+{
+    esp_err_t ret;
+    timer_config_t config = {
+        .divider = TIMER_DIVIDER,
+        .counter_dir = TIMER_COUNT_UP,
+        .counter_en = TIMER_START,
+        .alarm_en = TIMER_ALARM_EN,
+        .intr_type = TIMER_INTR_LEVEL,
+        .auto_reload = auto_reload,
+    };
+
+    // Inicia o timer (idx) do grupo 0 com as configs acima
+    ret = timer_init(TIMER_GROUP_0, timer_idx, &config);
+    ESP_ERROR_CHECK(ret);
+    // Seta o timer com 0x00000000 (ULL)
+    ret = timer_set_counter_value(TIMER_GROUP_0, timer_idx, 0x00000000ULL);
+    ESP_ERROR_CHECK(ret);
+    // Configura o alarme
+    ret = timer_set_alarm_value(TIMER_GROUP_0, timer_idx, ALARM_VAL_US);
+    ESP_ERROR_CHECK(ret);
+    // Habilita interrupcao
+    ret = timer_enable_intr(TIMER_GROUP_0, timer_idx);
+    ESP_ERROR_CHECK(ret);
+    // Registra a chamada da interrupcao
+    timer_isr_register(TIMER_GROUP_0, timer_idx, timer0_ISR, (void *)alarm_counter, ESP_INTR_FLAG_IRAM, NULL);
+}
 
 void app_main(void)
 {
-    int i = 0;
+    config_timer(TIMER_0, WITH_RELOAD);
     while (1) {
-        printf("[%d] Hello world!\n", i);
-        i++;
+        printf("[%d] Hello world!\n", alarm_counter);
         vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
 }
