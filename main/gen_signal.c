f3875960742341b5cfd0882f228ae9366518d07b
gkis <gabrielkis65@gmail.com>
2021-04-14 23:49:18 -0300

    ALTERAR - Arquivos que contem funcoes para a geracao do sinal

---
+++ main/gen_signal.c
@@ -0,0 +1,47 @@
+/**
+ *  This file contains functions used on analog signal generation
+ *
+ *  Gabriel Kis Silvestre
+ *
+ */
+
+#include "gen_signal.h"
+
+/**
+ * @brief Config Signal output GPIOs
+ *
+ * @note The output signal will be generated as an digital signal and converted by hardware
+ *
+ */
+void config_gpio(void)
+{
+    gpio_config_t config_gpio = {
+        //TODO: Configurar multiplas portas como output do gpio
+        //.pin_bit_mask = GPIO_SEL_23 | GPIO_SEL_22 | GPIO_SEL_1 | GPIO_SEL_3 | GPIO_SEL_21 | GPIO_SEL_19 | GPIO_SEL_18 | GPIO_SEL_5 ,
+        .pin_bit_mask = GPIO_SEL_0,
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLDOWN_DISABLE,
+        .pull_down_en = GPIO_PULLUP_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
+    };
+    gpio_config(&config_gpio);
+    REG_WRITE(GPIO_ENABLE_REG, BIT2);//Define o GPIO2 como saída
+}
+
+/**
+ * @brief Generate sine wave
+ *
+ * @note Expand to sine various formats (received via usb)
+ *
+ */
+void generate_sine_wave(void)
+{
+    //TODO: Receive as param buffer containing um period data
+    for(int i=0; i<SAMPLES_PER_PERIOD; i++){
+        //period_signal_buffer[i] = lookup_sine_table[i];
+        //printf("look_up_table_value - %d\n", period_signal_buffer[i]);
+    }
+    //REG_WRITE(GPIO_ENABLE_REG, BIT2);//Define o GPIO2 como saída
+}
+
+//TODO: MAKE A FUNCTION TO WRITE UINT8_T TO GPIO PORTS
---
+++ main/gen_signal.h
@@ -0,0 +1,21 @@
+#ifndef GEN_SIGNAL_H
+#define GEN_SIGNAL_H
+
+#include "driver/gpio.h"
+#include "pinout.h"
+
+#define SIGNAL_FREQ             40 * 1000   // output signal 40kHz
+#define SAMPLE_RATE             1000 * 1000 // Sample rate 1MSpS
+#define SAMPLES_PER_PERIOD      25          // 1MSpS/40kHz
+
+static const uint8_t lookup_sine_table[SAMPLES_PER_PERIOD+1] = {
+    0x80,0x9f,0xbd,0xd7,0xeb,0xf9,0xff,0xfd,
+    0xf3,0xe2,0xca,0xae,0x8f,0x70,0x51,0x35,
+    0x1d,0x0c,0x02,0x00,0x06,0x14,0x28,0x42,
+    0x60,0x80
+};
+
+void config_gpio(void);
+void generate_sine_wave(void);
+
+#endif
